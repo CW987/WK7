47b0785c534a2493bbbb53c7e8ca935425328dc9
Curtis Witter <inward-panting0f@icloud.com>
2025-01-30 15:15:09 +0000

    Page route added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <BrowserRouter>
         <div>
           <h1>Welcome to my website!</h1>
-          <Link to=""
+          <Link to="./components/UserPage">User page</Link>
+          <Routes>
+            <Route path="./componenets/UserPage" element={<h2>User Page</h2>} />
+          </Routes>
         </div>
       </BrowserRouter>
     </>
